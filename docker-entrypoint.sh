fd61579cd06bf0a93141d510acef88bf2fdcabf9
Ibsen Fahir Alves de Sousa <ibsenfahir@Ibsens-Macbook-Pro.local>
2019-05-13 10:18:00 -0300

    fix: update logs path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,7 @@ else
     python src/manage.py collectstatic --noinput  # Collect static files
 
     # Prepare log files and start outputting logs to stdout
-    rm -rf /src/logs
+    rm -rf /srv/logs
     mkdir /srv/logs/
     touch /srv/logs/gunicorn.log
     touch /srv/logs/access.log
